0be8ecda0ae5f2126eba5d42dba5e6a9e9e7685f
Rasheed963 <rasheedhaddad2018@gmail.com>
2025-06-29 12:09:53 +0300

    FGGHH

--- controllers/Auth_Controller.js
+++ controllers/Auth_Controller.js
@@ -1,18 +1,28 @@
 const Student = require("../models/Student");
 
 exports.signInStudent = async (req, res) => {
-  const { name, ID } = req.body;
-  const Student_Existance = await Student.findOne({ ID: ID, name: name });
+  try {
+    const { name, ID } = req.body;
 
-  if (!Student_Existance) {
-    const New_Student = new Student({
-      name: name,
-      ID: ID,
-      scores: [],
-    });
-    await New_Student.save();
-    return res.json({ user: New_Student });
-  } else if (Student_Existance) {
-    return res.json({ user: Student_Existance });
+    const existingStudent = await Student.findOne({ ID: ID });
+
+    if (!existingStudent) {
+      const newStudent = new Student({
+        name: name,
+        ID: ID,
+        scores: [],
+      });
+      await newStudent.save();
+      return res.json({ user: newStudent });
+    }
+
+    if (existingStudent.name !== name) {
+      return res.status(400).json({ error: "رقم جامعي مستخدم باسم مختلف" });
+    }
+
+    return res.json({ user: existingStudent });
+  } catch (err) {
+    console.error("SignIn Error:", err);
+    return res.status(500).json({ error: "خطأ في الخادم" });
   }
 };
