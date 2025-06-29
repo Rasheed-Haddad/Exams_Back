9879f540f47a2cf131b7958972fab984aa558462
Rasheed963 <rasheedhaddad2018@gmail.com>
2025-06-29 13:23:08 +0300

    ARRAYS

--- controllers/Exam_Controller.js
+++ controllers/Exam_Controller.js
@@ -5,7 +5,7 @@ exports.Get_Subjects = async (req, res) => {
   if (!Subjects_Existance) {
     return res.json("حاليا لا توجد أي اختبارات لهذا الفرع");
   }
-  return res.json([Subjects_Existance]);
+  return res.json(Subjects_Existance);
   /*const college_id = req.body.college_id;
   const New_Subject = new Subjects({
     name: "علم المناعة",
