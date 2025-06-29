ae1454b50b69081027ace2ee1022d1cbc3cd78ba
Rasheed963 <rasheedhaddad2018@gmail.com>
2025-06-29 12:00:48 +0300

    DDDDD

--- controllers/Auth_Controller.js
+++ controllers/Auth_Controller.js
@@ -1,7 +1,4 @@
-require("dotenv").config();
 const Student = require("../models/Student");
-const JWT = require("jsonwebtoken");
-const JWT_SECRET = process.env.JWT_SECRET;
 
 exports.signInStudent = async (req, res) => {
   const { name, ID } = req.body;
@@ -14,18 +11,8 @@ exports.signInStudent = async (req, res) => {
       scores: [],
     });
     await New_Student.save();
-    const token = JWT.sign({ ID, name }, JWT_SECRET, { expiresIn: "100d" });
-    return res.json({ user: New_Student, token });
+    return res.json({ user: New_Student });
   } else if (Student_Existance) {
-    const token = JWT.sign(
-      {
-        ID: Student_Existance.ID,
-        name: Student_Existance.name,
-        scores: Student_Existance.scores,
-      },
-      JWT_SECRET,
-      { expiresIn: "100d" }
-    );
-    return res.json({ user: Student_Existance, token });
+    return res.json({ user: Student_Existance });
   }
 };
