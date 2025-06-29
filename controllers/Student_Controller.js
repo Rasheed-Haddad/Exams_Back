7ca6e8f43a5dfd8b866a47fc9d4327b6c858c7d8
Rasheed <rasheedhaddad2018@gmail.com>
2025-06-29 17:44:59 +0300

    Update Student_Controller.js

--- controllers/Student_Controller.js
+++ controllers/Student_Controller.js
@@ -2,7 +2,18 @@ const Student = require("../models/Student");
 exports.Send_Student_Score = async (req, res) => {
   const { student_ID, subject_id, score } = req.body;
   const The_Student = await Student.findOne({ ID: student_ID });
-  await The_Student.user.scores.push({ subject_id: subject_id, score: score });
+
+  if (!The_Student) {
+    return res.status(404).json({ error: "الطالب غير موجود" });
+  }
+
+  // تأكد من وجود مصفوفة scores
+  if (!Array.isArray(The_Student.scores)) {
+    The_Student.scores = [];
+  }
+
+  The_Student.scores.push({ subject_id: subject_id, score: score });
   await The_Student.save();
+
   res.json(The_Student);
 };
