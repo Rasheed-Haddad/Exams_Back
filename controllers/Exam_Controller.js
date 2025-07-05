2179a016f1ffed9a800fc683b9ad4e61f9c15ea3
Rasheed963 <rasheedhaddad2018@gmail.com>
2025-07-05 08:23:41 +0300

    Get Only Available Exams

--- controllers/Exam_Controller.js
+++ controllers/Exam_Controller.js
@@ -1,9 +1,20 @@
 const Subjects = require("../models/Exam");
 exports.Get_Subjects = async (req, res) => {
-  const college_id = req.body.college_id;
-  const Subjects_Existance = await Subjects.find({ college_id: college_id });
-  if (!Subjects_Existance) {
-    return res.json("حاليا لا توجد أي اختبارات لهذا الفرع");
+  const { college_id, ID } = req.body;
+
+  try {
+    const filteredSubjects = await Subjects.find({
+      college_id: college_id,
+      available_to: ID, // Mongoose will check if the array includes this value
+    });
+
+    if (filteredSubjects.length === 0) {
+      return res.status(404).json("حاليًا لا توجد اختبارات متاحة لهذا الطالب.");
+    }
+
+    res.json(filteredSubjects);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json("حدث خطأ في الخادم.");
   }
-  return res.json(Subjects_Existance);
 };
